9544c96d390a089eb3fb90560519e9d9a6b65388
ttaerrim <ltr0121@naver.com>
2024-01-16 17:47:10 +0900

    feat: 시계 레이아웃 만들기
    - 시침, 분침, 초침 추가
    - 초침 로직 구현
    - 시계 전체적 레이아웃 스타일링 (css)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const secondRef = useRef<HTMLDivElement>(null);
+
+  const setDate = () => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = (seconds / 60) * 360 + 90;
+    if (secondRef.current) {
+      secondRef.current.style.transform = `rotate(${secondsDegrees}deg)`;
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(setDate, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='clock'>
+      <div className='clock-face'>
+        <div className='hand hour'></div>
+        <div className='hand minute'></div>
+        <div className='hand second' ref={secondRef}></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
